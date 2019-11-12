5c4c1f8e6d07bb88b81d0d4f818531748f38db56
Nate <nelliott1993@gmail.com>
2019-11-11 18:30:57 -0800

    implement Brewery model class

---
+++ src/main/java/model/Brewery.java
@@ -0,0 +1,283 @@
+package model;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * This is the model class for a Brewery.
+ */
+public class Brewery {
+    private int id;
+    private String name;
+    private String type;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private double longitude;
+    private double latitude;
+    private String phoneNumber;
+    private URL websiteURL;
+    private Date lastUpdated;
+    private List<String> tags;
+
+    /**
+     * Instantiates a new Brewery.
+     */
+    public Brewery() {
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets type.
+     *
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets type.
+     *
+     * @param type the type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Gets street address.
+     *
+     * @return the street address
+     */
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    /**
+     * Sets street address.
+     *
+     * @param streetAddress the street address
+     */
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    /**
+     * Gets city.
+     *
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * Sets city.
+     *
+     * @param city the city
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * Gets state.
+     *
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * Sets state.
+     *
+     * @param state the state
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * Gets postal code.
+     *
+     * @return the postal code
+     */
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    /**
+     * Sets postal code.
+     *
+     * @param postalCode the postal code
+     */
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    /**
+     * Gets country.
+     *
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * Sets country.
+     *
+     * @param country the country
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * Gets longitude.
+     *
+     * @return the longitude
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * Sets longitude.
+     *
+     * @param longitude the longitude
+     */
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    /**
+     * Gets latitude.
+     *
+     * @return the latitude
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Sets latitude.
+     *
+     * @param latitude the latitude
+     */
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * Gets phone number.
+     *
+     * @return the phone number
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * Sets phone number.
+     *
+     * @param phoneNumber the phone number
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * Gets website url.
+     *
+     * @return the website url
+     */
+    public URL getWebsiteURL() {
+        return websiteURL;
+    }
+
+    /**
+     * Sets website url.
+     *
+     * @param websiteURL the website url
+     */
+    public void setWebsiteURL(URL websiteURL) {
+        this.websiteURL = websiteURL;
+    }
+
+    /**
+     * Gets last updated.
+     *
+     * @return the last updated
+     */
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    /**
+     * Sets last updated.
+     *
+     * @param lastUpdated the last updated
+     */
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    /**
+     * Gets tags.
+     *
+     * @return the tags
+     */
+    public List<String> getTags() {
+        return tags;
+    }
+
+    /**
+     * Sets tags.
+     *
+     * @param tags the tags
+     */
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+}
