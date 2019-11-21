a9252ada10613d30afe4c66b04bc15b12e6b7825
Nate <nelliott1993@gmail.com>
2019-11-21 15:38:34 -0800

    add doPost method and remove code for getAllBreweries

--- src/main/java/servlets/BrewerySearchServlet.java
+++ src/main/java/servlets/BrewerySearchServlet.java
@@ -1,4 +1,35 @@
 package servlets;
 
-public class BrewerySearchServlet {
+import model.Brewery;
+import model.OpenBreweryDb;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet (
+        name = "brewerySearch",
+        urlPatterns = {"/brewerySearch"}
+)
+
+public class BrewerySearchServlet extends HttpServlet {
+    private OpenBreweryDb openBreweryDb;
+
+    public void init() {
+        openBreweryDb = new OpenBreweryDb();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        List<Brewery> breweries = new ArrayList<>();
+        breweries = openBreweryDb.getAllBreweries();
+    }
 }
