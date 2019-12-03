76a0875ddeb79ebba3c9e4e6ae5dd0c5d4f51189
Nate <nelliott1993@gmail.com>
2019-12-03 10:58:00 -0800

    clean up directory after changes to .gitignore

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm ~/apache-tomcat-9.0.29/webapps/BreweryFinder.war &&
-rm -r ~/apache-tomcat-9.0.29/webapps/BreweryFinder;
-cp target/BreweryFinder.war ~/apache-tomcat-9.0.29/webapps
