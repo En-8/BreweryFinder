f97446bb1f4582037f73c9860e73610de4d5da6d
Nate <nelliott1993@gmail.com>
2019-12-03 08:49:30 -0800

    settings

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm ~/apache-tomcat-9.0.27/webapps/BreweryFinder.war &&
-rm -r ~/apache-tomcat-9.0.27/webapps/BreweryFinder;
-cp target/BreweryFinder.war ~/apache-tomcat-9.0.27/webapps
+rm ~/apache-tomcat-9.0.29/webapps/BreweryFinder.war &&
+rm -r ~/apache-tomcat-9.0.29/webapps/BreweryFinder;
+cp target/BreweryFinder.war ~/apache-tomcat-9.0.29/webapps
